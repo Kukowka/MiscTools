7e7aa4c31e2014a3dc202a9c96cc47192eb75f18
Pawel Kukowka <Pawel.Kukowka@tms-at.com>
2024-01-10 15:23:54 +0100

    asd

--- ExcelProtectionRemover/ExcelProtectionRemover/Program.cs
+++ ExcelProtectionRemover/ExcelProtectionRemover/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.IO;
 
 namespace ExcelProtectionRemover
 {
     class Program
     {
-        public const string ROOT = @"e:\20231006\OneDrive_1_10-6-2023\WOB_K6BHIN";
+        public const string ROOT = @"e:\20231007\OneDrive_1_10-7-2023";
 
         static void Main(string[] args)
         {
-            var protectionRemover = new ExcelProtectionRemover();
-            protectionRemover.RemoveProtectionFromFile(@"e:\20231006\OneDrive_1_10-6-2023\WOB_K6BHIN\11_A33_31D_433574_WPS_BHIN_ST1430_V00_20230629.xlsm");
+            var allExcelFiles = Directory.GetFiles(ROOT, "*.xlsm", SearchOption.AllDirectories);
+
+            foreach (var excelFile in allExcelFiles)
+            {
+                Console.WriteLine(excelFile);
+                var protectionRemover = new ExcelProtectionRemover();
+                protectionRemover.RemoveProtectionFromFile(excelFile);
+            }
         }
 
 
