f232a09335c050bfb93ca523352c18cfce226e6f
Pawel Kukowka <Pawel.Kukowka@tms-at.com>
2024-03-05 17:30:45 +0100

    first version of postDownload finished

---
+++ AbbRapidPsRewrorker/AbbRapidPreUpload/IFileManager.cs
@@ -0,0 +1,16 @@
+﻿namespace AbbRapidPreUploader
+{
+    public interface IFileManager
+    {
+        string FileReadAllText(string filePath);
+
+        /// <summary>
+        /// Creates a backup copy of a file by changing its extension to '.backup'.
+        /// </summary>
+        /// <param name="filePath">The path to the file that needs to be backed up.</param>
+        /// <returns>The path to the created backup file.</returns>
+        string FileCreateFileBackup(string filePath);
+
+        void FileSaveAllText(string contents, string filePath);
+    }
+}
